bebe5db7a7e1107edd5fd02d4aa4c177885f895b
Ilene Gorski <ilene.gorski@gmail.com>
2017-07-07 10:56:48 -0700

    add form to create new package

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -5,4 +5,19 @@ class PackagesController < ApplicationController
   def show
     @package = Package.find(params[:id])
   end
+  def new
+    @package = Package.new
+  end
+  def create
+    @package = Package.new(package_params)
+    if @package.save
+      redirect_to  packages_path
+    else
+      render :new
+    end
+  end
+private
+  def package_params
+    params.require(:package).permit(:name, :description, :price)
+  end
 end
